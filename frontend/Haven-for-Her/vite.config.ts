df4e6e5017c0659bcc26b07b7567583bcbb753d1
ReadyPlayerSpork <lukeaaronmiller@gmail.com>
2026-04-06 21:44:26 -0600

    correctly configured shadcn css components for the project

--- frontend/Haven-for-Her/vite.config.ts
+++ frontend/Haven-for-Her/vite.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react, { reactCompilerPreset } from "@vitejs/plugin-react"
+import babel from "@rolldown/plugin-babel"
+import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss(),
     react(),
+    tailwindcss(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
